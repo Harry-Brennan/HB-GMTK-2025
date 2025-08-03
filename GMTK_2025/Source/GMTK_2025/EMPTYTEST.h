72051b0d0c4bac10f9380e86ea7b7f39e4b86f78
Harry Brennan <harrymb04@hotmail.co.uk>
2025-08-03 12:40:06 +0100

    Had to make an empty C++ class since blueprint only projects mess up with FMOD plugin packaging. Also updated the main menu and pause menu title cards to include "Dr Brawn's". Updated main menu and pause menu title positioning and animation positioning.

---
+++ GMTK_2025/Source/GMTK_2025/EMPTYTEST.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class GMTK_2025_API EMPTYTEST
+{
+public:
+	EMPTYTEST();
+	~EMPTYTEST();
+};
